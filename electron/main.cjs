a64b1430daad4937d4592cd192a79104742f68ec
Omniscient <omniscientcentral@gmail.com>
2026-09-18 02:59:22 -0300

    feat: run FOGA Eventos inside Electron

---
+++ electron/main.cjs
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1100,
+    minHeight: 700,
+    show: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  // Primera prueba: Electron muestra la app que ejecuta Vite localmente.
+  mainWindow.loadURL("http://localhost:8080");
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
